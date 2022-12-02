89925fbd36dea097534a77a42a42b7b4983e3a6d
DeV <debanjan.das90@gmail.com>
2022-12-02 22:28:30 +0530

    calculator works only for 2 numbers

--- operation.js
+++ operation.js
@@ -12,21 +12,55 @@ function operation(number,operator){
 
         inputArray.push(number,operator);
         console.log(`inputArray:${inputArray}`);
-    
+        
         operationLine.textContent = `${inputArray.toString().replace(/,/g,' ')}`;
 
-        if(numberArray.length > 1){
+        if(numberArray.length > 1){}
 
-            if (operator == '\u00d7'){     // unicode no. for multiply sign
-                let product = 1;
+            if(inputArray.includes('=')){
 
-                for (i = 0;i<numberArray.length; i++){
-                    product *= numberArray[i];
+                if (inputArray.includes('+')) {
+                    let sum = 0;
+                    for (i = 0; i < numberArray.length; i++) {
+                        sum += numberArray[i];
+                    }
+                    console.log(sum);
+                    userInputAndResult.textContent = sum;
+
+                }
+
+                if (inputArray.includes('-')) {
+                    let subtractResult = numberArray.reduce((previousNum, currentNum) => {
+                        return previousNum - currentNum;
+                    });
+
+                    console.log(subtractResult);
+                    userInputAndResult.textContent = subtractResult;
+
+                }
+
+                if (inputArray.includes('\u00d7')){     // unicode no. for multiply sign
+                    let product = 1;
+                    for (i = 0;i<numberArray.length; i++){
+                        product *= numberArray[i];
+                    }
+                    console.log(product);
+                    userInputAndResult.textContent = product;
+        
                 }
     
-                console.log(product);
+                if (inputArray.includes('\u00f7')) {   // unicode no. for division sign
+                    let divisionResult = numberArray.reduce((previousNum, currentNum) => {
+                        return previousNum / currentNum;
+                    });
+
+                    console.log(divisionResult);
+                    userInputAndResult.textContent = divisionResult;
+
+                }
             }
-        }
+            
+        
 
     }
 
