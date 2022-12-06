0ac8e1c607dc8e9883b7df9de0f20156fb8031fc
DeV <debanjan.das90@gmail.com>
2022-12-06 20:57:26 +0530

    infixtopostfix expression conversion logic made

--- expressionEvaluate.js
+++ expressionEvaluate.js
@@ -1,6 +1,6 @@
 const expression = '4^2*3-3+8/4/(1+1*(1+6))'
 
-let postfixExpr = [];
+let postfixExpression = [];
 let operatorStack = [];
 
 function infixToPostfixExpression(expression){
@@ -8,26 +8,43 @@ function infixToPostfixExpression(expression){
     let infixExpression = expression.split('');
 
     let precedenceOrder = {
-        '(' : 10,
-        '^' : 9,
-        '/' : 8,
-        '*' : 7,
-        '+' : 6,
+        '(' : 8,
+        '^' : 7,
+        '/' : 6,
+        '*' : 6,
+        '+' : 5,
         '-' : 5,
     }
     
     infixExpression.forEach( (symbol) => {
         if(parseInt(symbol)){
-            postfixExpr.push(symbol);
+            postfixExpression.push(symbol);
         }
 
         else{
             if (operatorStack.length != 0){
 
+                if(symbol === ')'){
+                    let temp = operatorStack.pop();
+                    operatorStack.push(temp);
+                    while(temp !== '('){
+                        temp = operatorStack.pop();
+                        postfixExpression.push(temp);
+                    }
+                    postfixExpression.pop();
+                    return;
+                }
+
+
                 let previousSymbol = operatorStack.pop();
+
+                if(previousSymbol === '('){
+                    operatorStack.push(previousSymbol,symbol);
+                    return;
+                }
     
-                if(precedenceOrder[previousSymbol] > precedenceOrder[symbol]){
-                    postfixExpr.push(previousSymbol);
+                if(precedenceOrder[previousSymbol] >= precedenceOrder[symbol]){
+                    postfixExpression.push(previousSymbol);
                     operatorStack.push(symbol);
                 }
                 else{
@@ -39,24 +56,15 @@ function infixToPostfixExpression(expression){
                 operatorStack.push(symbol);
             }
         }
-        // console.log(operatorStack);
+        console.log(operatorStack);
         
     } );
 
     while(operatorStack.length != 0){
-        postfixExpr.push(operatorStack.pop());
+        postfixExpression.push(operatorStack.pop());
     }
 
-    let postfixExpression = postfixExpr.filter( (symbol) => {
-        if (symbol === '(' || symbol === ')'){
-            return;
-        }
-        else{
-            return symbol;
-        }
-    } )
-
-    console.log(`postfixExpr:${postfixExpression}\noperatorstack:${operatorStack}`);
+    console.log(`postfixExpression:${postfixExpression}\noperatorstack:${operatorStack}`);
 }
 
-infixToPostfixExpression('4^2*3-3+8/4/(1+1*(1^6*(1/6)))');
+infixToPostfixExpression('4^2*3-3+8/4/(1+1*(1+6^(1/6)))');
